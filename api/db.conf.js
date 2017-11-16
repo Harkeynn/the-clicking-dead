dc29eeb2f83ecd1bd8718cc61dcd6da74e4e2603
maxime <maxime.pic@hotmail.fr>
2017-11-16 15:02:15 +0100

    add db.conf.js

---
+++ api/db.conf.js
@@ -0,0 +1,9 @@
+const databaseConf = {
+    DB_NAME: "clickingdead",
+    DB_LOGIN: "root",
+    DB_PASSWORD: "root",
+    DB_HOST: "localhost",
+    DB_DIALECT: "mysql"
+};
+
+module.exports = databaseConf;
