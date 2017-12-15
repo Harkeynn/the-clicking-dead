b490d580fb737f143573185d1fa71873c6796ed5
maxime <maxime.pic@hotmail.fr>
2017-12-15 11:10:39 +0100

    Fix account creation problem

--- api/models/account.js
+++ api/models/account.js
@@ -29,6 +29,7 @@ Account.sync().then(() => {
 		.then(account => {
 			if (account === null) {
 				Account.create({
+					id: 1,
 					nickname: 'Admin',
 					password: bcrypt.hashSync('Admin', null, null),
 					email: 'admin@ad.min',
